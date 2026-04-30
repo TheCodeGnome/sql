851de74ebeffc82ce9c16a72c3db8cdebe4fccf7
The Code Gnome <thecodegnome@outlook.com>
2026-04-29 22:00:31 -0400

    Assign 01 SQL sections 1 and 2

--- 02_activities/assignments/Microcredential_Cohort/assignment1.sql
+++ 02_activities/assignments/Microcredential_Cohort/assignment1.sql
@@ -6,7 +6,8 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 --QUERY 1
-
+SELECT *
+FROM customer;
 
 
 
@@ -16,7 +17,11 @@
 /* 2. Write a query that displays all of the columns and 10 rows from the customer table, 
 sorted by customer_last_name, then customer_first_ name. */
 --QUERY 2
-
+SELECT *
+FROM customer
+-- Wasn't sure if you wanted us to use case in-sensitive, as da Silva would be pushed to the end otherwise.
+ORDER BY customer_last_name COLLATE NOCASE, customer_first_name COLLATE NOCASE 
+LIMIT 10;
 
 
 
@@ -27,7 +32,10 @@ sorted by customer_last_name, then customer_first_ name. */
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. 
 Limit to 25 rows of output. */
 --QUERY 3
-
+SELECT *
+FROM customer_purchases
+WHERE product_id IN (4,9)
+LIMIT 25;
 
 
 
@@ -42,9 +50,10 @@ filtered by customer IDs between 8 and 10 (inclusive) using either:
 Limit to 25 rows of output.
 */
 --QUERY 4
-
-
-
+SELECT *, (quantity * cost_to_customer_per_qty) as price
+FROM customer_purchases
+WHERE customer_id BETWEEN 8 AND 10
+LIMIT 25;
 
 --END QUERY
 
@@ -55,7 +64,12 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 --QUERY 5
-
+SELECT product_id, product_name, 
+	CASE 
+		WHEN product_qty_type = "unit" THEN "unit"
+		ELSE "bulk"
+	END	AS prod_qty_type_condensed
+FROM product
 
 
 
@@ -66,7 +80,17 @@ if the product_qty_type is “unit,” and otherwise displays the word “bulk.�
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 --QUERY 6
-
+SELECT product_id, product_name, 
+	CASE 
+		WHEN product_qty_type = "unit" THEN "unit"
+		ELSE "bulk"
+	END	AS prod_qty_type_condensed,
+	CASE
+		-- Could have used "%pepper%" or a similar term, but decided to be mindful of plurals and potential false positives
+		WHEN product_name LIKE "% pepper%" COLLATE NOCASE THEN 1
+		ELSE 0
+	END AS pepper_flag
+FROM product
 
 
 
@@ -77,8 +101,12 @@ contains the word “pepper” (regardless of capitalization), and otherwise out
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by market_date, then vendor_name.
 Limit to 24 rows of output. */
---QUERY 7
-
+--QUERY 7 
+SELECT vb.*, v.*
+FROM vendor AS v
+	JOIN vendor_booth_assignments AS vb ON v.vendor_id = vb.vendor_id
+ORDER BY market_date, vendor_name
+LIMIT 24;
 
 
 
